6292803331cd5185afa6e019cfb890e103d12902
judge2010 <74088083+judge2010@users.noreply.github.com>
2021-02-13 12:00:35 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -9,8 +9,26 @@ Webcam.attach(camera);
 
 function Take_photo(){
     Webcam.snap(function(data_uri){
-        document.getElementById("snapshot").innerHTML = "<img src='"+data_uri+"'>"
+        document.getElementById("snapshot").innerHTML = "<img id='Photo' src='"+data_uri+"'>"
     } )
 };
 console.log("ml5",ml5.version);
-classifier=ml5.imageclassifier("https://teachablemachine.withgoogle.com/models/rcQWm8Ixq/model.json",modelLoaded);
+classifier=ml5.imageClassifier("https://teachablemachine.withgoogle.com/models/rcQWm8Ixq/model.json",modelLoaded);
+function modelLoaded(){
+ console.log("model has been loaded");
+};
+function Identify(){
+ var img=document.getElementById("Photo");
+ classifier.classify(img,gotResult);
+};
+function gotResult(error,result){
+    if(error){
+        console.log(error);
+    }
+    else{
+    console.log(result);
+    
+    document.getElementById("result_of_prediction1").innerHTML= result[0].label;
+    document.getElementById("result_of_prediction2").innerHTML= result[1].label;
+    }
+}
